cb5fcf2def64852c26a8d01160a4d4711fde61a0
Federico Lancerin <federico.lancerin@gmail.com>
2021-05-02 13:10:27 +0200

    ex 1_6

---
+++ chapter_1/1_6_ex_c.hs
@@ -0,0 +1,15 @@
+
+a = 2001
+
+b = "2001"
+
+c = 'H'
+
+d = "H"
+
+
+main = do
+  print ([1,2,3] ++ [3,2,1])
+  print ("Hello" ++ " World!")
+  print ([1,2,3] ++ [])
+  print ("Hello" ++ "" ++ "World!")
